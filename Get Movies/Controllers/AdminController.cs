3f026bd012456df6dd8a7428f54769116b2a2756
AhmedMaad <35191573+AhmedMaad@users.noreply.github.com>
2019-04-15 19:53:31 +0200

    Remove Movie View design and implementation is ready with a bug

--- Get Movies/Controllers/AdminController.cs
+++ Get Movies/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
@@ -11,23 +12,36 @@ namespace Get_Movies.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
-         
+
         public ActionResult addmovie()
-        { 
+        {
             return View();
         }
 
         [HttpPost]
         public ActionResult addmovie(Movie m)
         {
-            m.Rating_Count = 0;
-            m.View_Count = 0;
-            m.Genre_Id = 3;
-
-            m.Add();
-
-
+                m.Rating_Count = 0;
+                m.View_Count = 0;
+                m.Genre_Id = 3;
+                m.Add();
             return View();
         }
+
+        public ActionResult deletemovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult deletemovie(Movie m)
+        {
+            m.Remove(false, false);
+            Debug.WriteLine("xxxxxxxxxxxx" + m.Title);
+            Debug.WriteLine("xxxxxxxxxxxx" + m.Search(false, true) == null);
+            return View();
+        }
+
+
     }
 }
